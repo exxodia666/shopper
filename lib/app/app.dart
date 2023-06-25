ed78681b70b097393ef0ad2f7d8299d4f0435334
spike <ololmkbc.dev@gmail.com>
2023-06-25 19:10:26 +0300

    feat: setup firebase auth sdk

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:shopper/src/navigation/main_navigator.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MainNavigator();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shopper/src/navigation/main_navigator.dart';
+import 'package:shopper/app/app.dart';
+import 'package:shopper/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MainNavigator();
-  }
-}
-
