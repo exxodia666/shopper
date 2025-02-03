609393030cdc9440f55f425d4b1f0f42f62962a6
exxodia666 <ololmkbc@gmail.com>
2025-02-04 01:48:31 +0200

    remove skeletonizer, add loader

--- lib/widgets/grid_product_list/grid_product_list.dart
+++ lib/widgets/grid_product_list/grid_product_list.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopper/app/bloc/app_bloc.dart';
 import 'package:shopper/models/models.dart';
-import 'package:skeletonizer/skeletonizer.dart';
+
 import '../grid_product_item/grid_product_item.dart';
 
 class GridProductList extends StatelessWidget {
@@ -22,61 +22,52 @@ class GridProductList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //todo remove this
     var user = context.select((AppBloc bloc) => bloc.state.user);
     return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 0.0),
-        child: Skeletonizer(
-          enabled: isLoading,
-          child: GridView.builder(
-            primary: false,
-            shrinkWrap: true,
-            padding: const EdgeInsets.only(
-              bottom: 120,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 0.0),
+            child: GridView.builder(
+              primary: false,
+              shrinkWrap: true,
+              padding: const EdgeInsets.only(
+                bottom: 120,
+              ),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 300,
+                  childAspectRatio: 3.1 / 4,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return GridProductItem(
+                  item: item,
+                  onProductPress: () {
+                    onProductPress(item.id);
+                  },
+                  onCartPress: () {
+                    onCartPress(item, user.id);
+                  },
+                  onFavoritePress: () {
+                    onFavoritePress(item, user.id);
+                  },
+                );
+              },
             ),
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 300,
-                childAspectRatio: 3.1 / 4,
-                crossAxisSpacing: 20,
-                mainAxisSpacing: 20),
-            itemCount: items.length,
-            itemBuilder: (context, index) {
-              final item = items[index];
-              return GridProductItem(
-                item: item,
-                onProductPress: () {
-                  onProductPress(item.id);
-                },
-                onCartPress: () {
-                  onCartPress(item, user.id);
-                },
-                onFavoritePress: () {
-                  onFavoritePress(item, user.id);
-                  // Todo add animation on this
-                },
-              );
-            },
           ),
-        ),
+          if (isLoading)
+            const Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 80,
+              // todo custom loader
+              child: Center(child: CircularProgressIndicator()),
+            ),
+        ],
       ),
     );
   }
 }
-
-// showDialog(
-                //     context: context,
-                //     builder: (context) => BlocBuilder<CartProdCountCubit, int>(
-                //           builder: (context, state) {
-                //             return AddToCartModal(
-                //                 item: item,
-                //                 counter: state,
-                //                 reset: context.read<CartProdCountCubit>().reset,
-                //                 increment: context
-                //                     .read<CartProdCountCubit>()
-                //                     .increment,
-                //                 decrement: context
-                //                     .read<CartProdCountCubit>()
-                //                     .decrement);
-                //           },
-                //         ));
